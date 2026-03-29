edebae48ad49f7c20b05ec084b766ccf4e2708ba
Si Rayan <ahmilano310@gmail.com>
2026-03-29 18:39:47 +0100

    Update 03-29

--- Learn-API/EX3/app.js
+++ Learn-API/EX3/app.js
@@ -1,23 +1,20 @@
-//!
-
-
-
-
 let myRequast = new XMLHttpRequest();
-myRequast.open("GET", 'https://api.github.com/users/BM-NAPOLI');
-myRequast.send()
-console.log(myRequast);
+myRequast.open("GET", 'https://api.github.com/users/BM-NAPOLI/repos');
+myRequast.send();
 
 myRequast.onreadystatechange = function() {
-    // console.log(myRequast.readyState);
-    // console.log(myRequast.status);
     if(this.readyState === 4 && this.status === 200){
-        // console.log(this.responseText);
-    let jsData = JSON.parse(this.responseText);
+        
+        let jsData = JSON.parse(this.responseText);
+
         for(let i = 0; i < jsData.length; i++){
             let div = document.createElement("div");
+
             let repoName = document.createTextNode(jsData[i].full_name);
+            let stars = document.createTextNode("Stars: " + " ⭐ " + jsData[i].stargazers_count);
+
             div.appendChild(repoName);
+            div.appendChild(stars);
             document.body.appendChild(div);
         }
     }

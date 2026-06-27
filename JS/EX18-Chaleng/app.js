2577941acfa9688e8e4ba915f23bcd3518e97a3d
Si Rayan <ahmilano310@gmail.com>
2026-06-27 03:28:34 +0100

    Uppdate 2026-06-27 | 3:28

--- JS/EX18-Chaleng/app.js
+++ JS/EX18-Chaleng/app.js
@@ -3,3 +3,29 @@
 
 
 
+let myAdmins = ["Ahmed", "Ossama", "Sayed", "Stop", "Samera"];
+let myEmployees = ["Amged", "Sameh", "Ammer", "Omar", "Othman", "Amany", "Samia", 'Anwar'];
+
+for (let i = 0; i < myAdmins.length; i++) {
+
+    if (myAdmins[i] === "Stop") {
+        document.write(`<div>We have ${i} Admins :</div>`);
+        document.write(`<hr>`);
+        break;
+    }
+}
+for (let j = 0; j < myAdmins.length; j++) {
+    if (myAdmins[j] === "Stop") {
+        break;
+    }
+    document.write(`<div><H4>The Admin For Team ${j + 1} Is ${myAdmins[j]} :</H4></div>`);
+    document.write(`<div><H2>Teams Memberes :</H2></div>`);
+    let conter = 1;
+    for (let k = 0; k < myEmployees.length; k++) {
+        if (myAdmins[j][0] === myEmployees[k][0]) {
+            document.write(`<div>- ${conter} ${myEmployees[k]}</div>`);
+            conter++;
+        }
+    }
+    document.write(`<hr>`);
+}   
